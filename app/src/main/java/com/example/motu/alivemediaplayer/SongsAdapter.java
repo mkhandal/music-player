a6486c048ba9cf4bc75ea6f47619c7e43e87e572
vijaynitrr <vijaykhandalnitrr@gmail.com>
2018-02-09 22:12:03 +0530

    add files

---
+++ app/src/main/java/com/example/motu/alivemediaplayer/SongsAdapter.java
@@ -0,0 +1,8 @@
+package com.example.motu.alivemediaplayer;
+
+/**
+ * Created by motu on 9/2/18.
+ */
+
+public class SongsAdapter {
+}
---
+++ app/src/main/java/com/example/motu/alivemediaplayer/SongsInfo.java
@@ -0,0 +1,14 @@
+package com.example.motu.alivemediaplayer;
+
+/**
+ * Created by motu on 9/2/18.
+ */
+
+public class SongsInfo {
+    public String SongName, ArtistName, SongUrl;
+
+    public SongsInfo(){
+    }
+
+
+}
